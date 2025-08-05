f38d0789c2db382d6761e2bbb427d656d21d8d46
Sandro-Tk <sandrotkemaladze2002@gmail.com>
2025-08-05 20:36:39 +0400

    prepared app for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 const notesRoutes = require("./routes/noteRoutes");
 const { connectDB } = require("./config/db");
@@ -12,15 +13,24 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if (process.env.NODE_ENV !== "production")
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
 app.use(express.json()); // this middleware parses req.body
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
